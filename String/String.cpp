2795ea31ee1b6e125382b6eb378f0b99171fad1a
Dmitriy <dvol72@mail.ru>
2021-12-19 15:34:31 +0300

    Added += operator, overloaded cin method

--- String/String.cpp
+++ String/String.cpp
@@ -15,27 +15,45 @@ public:
     {
         return str;
     }
-    int get_size()
+    int get_size()const
     {
         return size;
     }
+    const char* get_str()const
+    {
+        return str;
+    }
+    
 
-    String(int size=80)
+    explicit String(int size=80)
     {
         this->size = size;
-        this->str[size];
-        cout << "Default constructor:\t" << this << endl;
+        this->str = new char[size] {};
+        cout <<  "Constructor:\t" << this << endl;
     }
+    
     String(const char* str)
     {
-        this->size = strlen(str);
-        this->str = new char[size];
+        this->size=strlen(str)+1;
+        this->str = new char[size] {};
         for (size_t i = 0; i < size; i++)
         {
             this->str[i] = str[i];
         }
         cout << "1argConstructor:\t" << this << endl;
     }
+
+    String(const String& other)
+    {
+        this->size = other.size;
+        this->str = new char[size] {};
+        for (size_t i = 0; i < size; i++)
+        {
+            this->str[i] = other.str[i];
+        }
+        cout << "CopyConstructor" << this << endl;
+    }
+
     ~String()
     {
         delete[]str;
@@ -48,38 +66,96 @@ public:
             this->str[i] = str[i];
         }
     }
-    void Print()
+    void print()const
     {
+        cout << size << endl;
+        cout << str << endl;
+    }
+
+    char& operator[](int i)
+    {
+        return str[i];
+    }
+
+    const char& operator[](int i)const
+    {
+        return str[i];
+    }
+
+    String& operator=(const String& other)
+    {
+        if (this == &other)return *this;
+        delete[] this->str;
+        this->size = other.size;
+        this->str = new char[size] {};
         for (size_t i = 0; i < size; i++)
         {
-            cout << str[i];
+            this->str[i] = other.str[i];
         }
-        cout << endl;
+        cout << "CopyAssigment" << this << endl;
+        return *this;
     }
+
 };
 
-String operator+(String& str1, String& str2)
+String operator+(const String& str1, const String& str2)
 {
-    String result=str1.get_size()+str2.get_size();
+    String result(str1.get_size() + str2.get_size()-1);
     for (size_t i = 0; i < str1.get_size(); i++)
     {
-        result.get_str()[i] = str1.get_str()[i];
+        result[i] = str1[i];
     }
     for (size_t i = 0; i < str2.get_size(); i++)
     {
-        result.get_str()[i + str1.get_size()] = str2.get_str()[i];
+        result[i + str1.get_size()-1] = str2[i];
     }
     return result;
 }
 
+String& operator+=(String& str1, const String& str2)
+{
+    return str1 = str1 + str2;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+    return os << obj.get_str();
+}
+
+std::istream& operator>>(std::istream& os, String& obj)
+{
+    os >> obj.get_str();
+    return os;
+}
+
+
+//#define CONSTRUCTORS_CHECK
+
 int main()
 {
-    String str;	//Пустая строка размером 80 Байт
-    String str1 = "Hello";
+#ifdef CONSTRUCTORS_CHECK
+    String str;
+    str.print();
+    String str2 = "Hello";
+    cout << str2 << endl;
+    String str4 = str2;
+    cout << str4 << endl;
+    String str5;
+    str5 = str2;
+    cout << str5 << endl;
+    str2 = str2;
+    cout << str2 << endl;
+#endif // CONSTRUCTORS_CHECK
+    /*String str1 = "Hello";
     String str2 = "World";
-    str2.Print();
-    String str3 = str1 + str2;
-    str3.Print();
+    str1 += str2;
+    str1.print();*/
+
+    String str;
+    cout << "enter words" << endl; 
+    cin >> str;
+    cout << str << endl;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
